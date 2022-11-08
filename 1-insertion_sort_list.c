85ea71a1838996f42b2acd7ed2e2818e98b5b2ab
koredeycode <koredey4u@gmail.com>
2022-11-08 17:14:42 +0000

    the implementation of the insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "sort.h"
+/**
+ * insertion_sort_list - sort the list using the insertion sort algorithm
+ * @list: the pointer to the head of the list to be sorted
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *node = NULL, *tmp = NULL;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	node = (*list)->next;
+
+	while(node)
+	{
+		while (node->prev && node->n < node->prev->n)
+		{
+			tmp = node;
+			if (node->next)
+				(node->next)->prev = tmp->prev;
+			(node->prev)->next = tmp->next;
+			node = node->prev;
+			tmp->prev = node->prev;
+			tmp->next = node;
+			if (node->prev)
+				(node->prev)->next = tmp;
+			node->prev = tmp;
+			if (tmp->prev == NULL)
+				*list = tmp;
+			print_list(*list);
+			node = node->prev;
+		}
+		node = node->next;
+	}
+}
